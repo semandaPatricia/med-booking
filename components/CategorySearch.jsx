40a9a55495516a2d3c1261fca8d278b78f2f560e
semanda <patash248@gmail.com>
2024-04-19 21:41:34 +0200

    contact and footer

--- components/CategorySearch.jsx
+++ components/CategorySearch.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-
-
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Search } from 'lucide-react'
 
 const CategorySearch = () => {
   return (
@@ -10,8 +11,14 @@ const CategorySearch = () => {
         Search <span className='text-green-600'>Doctor </span>
       </h2>
       <h2 className='text-gray-500'> search doctors Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, sed alias illo consequuntur beatae voluptate? Quasi quas magnam necessitatibus! Impedit sapiente voluptates quasi tempora nam porro. Perspiciatis officia amet quos.</h2>
-      <input type="search" placeholder="search doctor or department" />
-      <button className='rounded-full outline p-2'>search</button>
+      <div className='flex w-full max-w-sm items-center mt-3 gap-2 '>
+      <Input type="text" placeholder="search..." />
+      <Button type="submit" >
+      <Search className='h-4 w-4' />
+      Search
+      </Button>
+      </div>
+     
    
   </div>
   )
